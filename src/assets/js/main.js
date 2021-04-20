44020c0f929ce5f401cbda4352543d41fc025962
LAPTOP-N11PRJSU\maeco <maecolasse@outlook.com>
2021-04-20 18:23:44 +0200

    FEAT: pictures add, css pages add

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,41 @@
-console.log('I am not a penguin');
+"use strict";
+
+/*dark mode*/
+let activeTheme = localStorage.getItem("theme");
+if(activeTheme !==null){
+  setTheme(activeTheme);
+}
+let themeDarkmode = document.querySelector(".js-theme-darkmode");
+if(themeDarkmode){     themeDarkmode.addEventListener("click", toggleTheme);
+}
+function setTheme(theme){
+  document.body.setAttribute("color-theme", theme);
+  localStorage.setItem("theme",theme);
+}
+function toggleTheme(){
+  let theme = document.body.getAttribute("color-theme");
+  if(theme == "sombre"){
+    setTheme("clair");
+  }else{
+    setTheme("sombre");
+  }
+}
+
+/*burger menu*/
+let menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", toggleNavigation);
+
+function toggleNavigation(){
+    if(document.body.hasAttribute("burger-menu")){
+        document.body.removeAttribute("burger-menu");
+        menuButton.innerHTML = "Menu";
+    }else{
+        document.body.setAttribute("burger-menu", true);
+        menuButton.innerHTML = "Retour";
+    }
+}
+/*date*/
+let giveDate = document.querySelector(".copyright");
+let maintenant = new Date();
+let an = maintenant.getFullYear();
+giveDate.innerHTML = an;
